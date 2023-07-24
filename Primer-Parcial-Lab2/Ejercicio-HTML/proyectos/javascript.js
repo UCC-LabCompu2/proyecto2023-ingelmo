4c1b63106998a5628e9f6f1e11f2782b3c9f2336
Francisco.Ingelmo <franciscoingelmo@hotmail.com>
2023-07-23 21:15:51 -0300

    Se esta arreglando el proyecto

--- Primer-Parcial-Lab2/Ejercicio-HTML/proyectos/javascript.js
+++ Primer-Parcial-Lab2/Ejercicio-HTML/proyectos/javascript.js
@@ -34,7 +34,7 @@ let dibujarPendulo = () => {
     //Dibujo del cuadrado 0 (Es para que cuando se muestran valores en el canvas se borren y se ingrese los nuevos correctamente)
     ctx.beginPath();
     ctx.fillStyle = "#ffffff"
-    ctx.fillRect(45 + margen, alturaMax - 200 - margen, 400, 100);
+    ctx.fillRect(45 + margen, alturaMax - 200 - margen, 400, 120);
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
@@ -42,7 +42,7 @@ let dibujarPendulo = () => {
     //Dibujo del cuadrado 1
     ctx.beginPath();
     ctx.fillStyle = "#36b2c2"
-    ctx.fillRect(180 + margen, alturaMax - 200 - margen, 150, 100);
+    ctx.fillRect(180 + margen, alturaMax - 165 - margen, 150, 100);
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
@@ -58,8 +58,8 @@ let dibujarPendulo = () => {
     //Dibujo del cuadrado 3
     ctx.beginPath();
     ctx.fillStyle = "#061d21"
-    ctx.fillRect(220 + margen, 90 - margen, 1, alturaMax - 290);
-    ctx.fillRect(290 + margen, 90 - margen, 1, alturaMax - 290);
+    ctx.fillRect(220 + margen, 90 - margen, 1, alturaMax - 255);
+    ctx.fillRect(290 + margen, 90 - margen, 1, alturaMax - 255);
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
@@ -67,14 +67,44 @@ let dibujarPendulo = () => {
     //Dibujo de la bala
     ctx.beginPath();
     ctx.fillStyle = "#2a3131"
-    ctx.arc(90 + margen, alturaMax - 154, 3.5, 0, 2*Math.PI);
-    ctx.fillRect(70 + margen, alturaMax - 158, 20, 8);
+    ctx.arc(90 + margen, alturaMax - 114, 3.5, 0, 2*Math.PI);
+    ctx.fillRect(70 + margen, alturaMax - 118, 20, 8);
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
 
 }
 
+
+/**
+ * Permite animar un auto en un canvas
+ * @method animarAuto
+ */
+/* FALTA COMPLETAR
+var x= 0;
+var y = 100;
+var dx= 2;
+let animarAuto = () => {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    let img;
+    img = new Image();
+    img.src = "../imagenes/bala.png";
+
+    img.onload = function (){
+        canvas.width = canvas.width;
+        ctx.drawImage(img, x, y);
+    }
+    x+=dx;
+    if(x>=canvas.width){
+        x=0;
+        y=y+50;
+    }
+
+
+}
+*/
 /**
  * Calcula la altura final que va a tener el bloque colgado luego de recibir los datos ingresados. Aparte en el canvas va a mostrar los distintos datos ingresados en sus respectivos dibujos
  * @method calcularOp
@@ -109,9 +139,9 @@ let calcularOp = () => {
         ctx.beginPath();
         ctx.font="11pt Verdana";
         ctx.fillStyle = "#050000";
-        ctx.fillText( "m1:", 60, 285);
-        ctx.fillText( masa1, 90, 285);
-        ctx.fillText( "g", 123, 285);
+        ctx.fillText( "m1:", 60, 225);
+        ctx.fillText( masa1, 90, 225);
+        ctx.fillText( "g", 123, 225);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
@@ -120,9 +150,9 @@ let calcularOp = () => {
         ctx.beginPath();
         ctx.font="11pt Verdana";
         ctx.fillStyle = "#050000";
-        ctx.fillText( "v1:", 60, 320);
-        ctx.fillText( vel1, 90, 320);
-        ctx.fillText( "m/s", 123, 320);
+        ctx.fillText( "v1:", 60, 260);
+        ctx.fillText( vel1, 90, 260);
+        ctx.fillText( "m/s", 123, 260);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
@@ -131,11 +161,12 @@ let calcularOp = () => {
         ctx.beginPath();
         ctx.font="11pt Verdana";
         ctx.fillStyle = "#050000";
-        ctx.fillText( "m2:", 230, 300);
-        ctx.fillText( masa2, 260, 300);
-        ctx.fillText( "g", 293, 300);
+        ctx.fillText( "m2:", 230, 240);
+        ctx.fillText( masa2, 260, 240);
+        ctx.fillText( "g", 293, 240);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
     }
 }
+
